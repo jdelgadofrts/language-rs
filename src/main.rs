98347b33372bc3568e6c9a41fecabcb71706ff97
Juan Delgado <juan.delgado@fortisgames.com>
2024-10-23 15:11:55 -0700

    Add basic parsing of the expressions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,62 @@
+use chumsky::prelude::*;
+
+#[derive(Debug)]
+enum Expr {
+    Num(f64),
+    Var(String),
+
+    Neg(Box<Expr>),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+
+    Call(String, Vec<Expr>),
+    Let {
+        name: String,
+        rhs: Box<Expr>,
+        then: Box<Expr>,
+    },
+    Fn {
+        name: String,
+        args: Vec<String>,
+        body: Box<Expr>,
+        then: Box<Expr>,
+    },
+}
+
+fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
+    let int = text::int(10)
+        .map(|s: String| Expr::Num(s.parse().unwrap()))
+        .padded();
+
+    int.then_ignore(end())
+}
+
+fn eval(expr: &Expr) -> Result<f64, String> {
+    match expr {
+        Expr::Num(x) => Ok(*x),
+        Expr::Neg(a) => Ok(-eval(a)?),
+        Expr::Add(a, b) => Ok(eval(a)? + eval(b)?),
+        Expr::Sub(a, b) => Ok(eval(a)? - eval(b)?),
+        Expr::Mul(a, b) => Ok(eval(a)? * eval(b)?),
+        Expr::Div(a, b) => Ok(eval(a)? / eval(b)?),
+        _ => todo!(), // We'll handle other cases later
+    }
+}
+
 fn main() {
-    println!("Hello World");
+    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+
+
+    match parser().parse(src) {
+        Ok(ast) => match eval(&ast) {
+            Ok(output) => println!("{}", output),
+            Err(eval_err) => println!("Evalutation error: {}", eval_err),
+        }
+        Err(parse_errs) => parse_errs
+            .into_iter()
+            .for_each(|e| println!("Parse error: {}", e)),
+    }
 }
 
